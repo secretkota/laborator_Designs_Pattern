d897bd54fbe1b46bab960cfae3a3c08ae95bd98c
secretkota <stasbro123@gmail.com>
2025-09-16 19:00:48 +0300

    firstEdition

---
+++ laborator_1/index.ts
@@ -0,0 +1,61 @@
+
+type LessonType = "Math" | "Physics" | "Chemistry" | "History" | "Literature"
+
+class Student {
+    firstname: string;
+    lastname: string
+    age: number;
+    lesson: LessonType;
+    grade: number;
+
+    constructor(firstname: string, lastname: string, age: number, lesson: LessonType, grade: number) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.lesson = lesson;
+        this.grade = grade;
+    }
+}
+
+
+class FieldMask {
+    showFirstname: boolean = true;
+    showLastname: boolean = true;
+    showAge: boolean = true;
+    showLesson: boolean = true;
+    showGrade: boolean = true;
+}
+
+class StudentDB {
+    private students: Student[] = []
+
+    add(student: Student) {
+        this.students.push(student)
+    }
+
+    findByFirstName(name: string): Student[] {
+        return this.students.filter(stud => stud.firstname === name)
+    }
+
+    static print(student: Student, mask: FieldMask) {
+        if (mask.showFirstname) console.log(`FirstName: ${student.firstname}`)
+        if (mask.showLastname) console.log(`LastName: ${student.lastname}`)
+        if (mask.showAge) console.log(`Age: ${student.age}`)
+        if (mask.showLesson) console.log(`Lesson: ${student.lesson}`)
+        if (mask.showGrade) console.log(`ShowGrade: ${student.grade}`)
+
+    }
+}
+
+
+const db = new StudentDB()
+db.add(new Student('Alex', 'Smith', 20, "Literature", 9.5));
+
+
+const mask = new FieldMask()
+mask.showAge = false
+mask.showLastname = false
+
+const found = db.findByFirstName('Alex')
+found.forEach(s=> StudentDB.print(s, mask))
+
