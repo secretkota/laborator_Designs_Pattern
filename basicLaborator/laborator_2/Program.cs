fcd36420217a098e47516a62bf31831af837f0d2
secretkota <stasbro123@gmail.com>
2025-09-24 16:48:40 +0300

    Feat: Delete lab_1 design, add laborator 2 basic

---
+++ basicLaborator/laborator_2/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Console.WriteLine("Hello, Jerry");
+printABC();
+Thread.Sleep(500);
+printABC();
+Thread.Sleep(500);
+printABC();
+
+void printABC()
+{
+    Console.WriteLine("A");
+    Console.WriteLine("B");
+    Console.WriteLine("C");
+}
+
+void functionA()
+{
+    functionB();
+    functionC();
+}
+
+void functionB()
+{
+    Console.WriteLine("Function B");
+}
+
+void functionC()
+{
+    Console.WriteLine("Function C");
+}
+
+void functionD()
+{
+    Console.WriteLine("Function D");
+}
+
+functionA();
+Thread.Sleep(500);
+functionA();
+Thread.Sleep(500);
+functionA();
+Thread.Sleep(500);
