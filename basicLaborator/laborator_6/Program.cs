1b9821375a065b616f4f29aaa57a60bf6a31e4e7
secretkota <stasbro123@gmail.com>
2025-09-30 17:45:52 +0300

    ADD: Added laborator_6

---
+++ basicLaborator/laborator_6/Program.cs
@@ -0,0 +1,10 @@
+﻿var a = new List<int>() { 10, 11, 23, 15 };
+
+var b = new List<int>() { 1, 11, 23, 15 };
+var c = 0;
+
+for (var i = 0; i < a.Count; i++)
+{
+    c = a[i] + b[i];
+    Console.WriteLine(c);
+}
